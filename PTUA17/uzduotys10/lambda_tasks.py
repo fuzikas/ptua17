a223439199b39eb3ca55cd688a370b5014a8fdc4
Arnas <arnas.zmejauskas1@gmail.com>
2025-02-12 18:44:57 +0200

    lambdos

---
+++ PTUA17/uzduotys10/lambda_tasks.py
@@ -0,0 +1,10 @@
+# 1) Create a lambda function that takes a number and returns its square.
+# 2) Create a lambda function that takes two numbers and returns their squared sum.
+# 3) Use the lambda function to sort list of tuples based on the second element:
+# tuples_list = [(1,3), (4,1), (5,2), (2,4)]
+
+# 1
+square = lambda num_1: num_1 * num_1
+print(square(3))
+
+# 2
