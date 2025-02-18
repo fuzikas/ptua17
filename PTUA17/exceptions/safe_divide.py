c19f998b2e7a1119b1cc0d2b16125b80b96b1670
Arnas <arnas.zmejauskas1@gmail.com>
2025-02-18 19:17:04 +0200

    fixed

--- PTUA17/exceptions/safe_divide.py
+++ PTUA17/exceptions/safe_divide.py
@@ -12,15 +12,11 @@ def safe_divide(first_number:int, second_number:int)->float:
 
     try:
         result = first_number/second_number
+        print("Attempted division")
+        return result
     except ZeroDivisionError:
         logging.critical(f"No go, zero division error {ZeroDivisionError} ")
         print("Division by 0 is not possible")
     except TypeError:
         logging.critical(f"No go, one or both of the arguments != number {TypeError} ")
         print("both arguments must be numbers")
-    else:
-        logging.info(f"Divission successful and the result is {result}")
-        print("Division successful")
-        return result
-    finally:
-        print("Attempted division")
