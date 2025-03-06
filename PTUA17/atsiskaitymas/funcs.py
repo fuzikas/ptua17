8bf7c0de22211a3d99f1c99ce2c80ecc57d8c7b5
Arnas <arnas.zmejauskas1@gmail.com>
2025-03-06 17:01:47 +0200

    hello_sir

---
+++ PTUA17/atsiskaitymas/funcs.py
@@ -0,0 +1,57 @@
+import json
+from operator import itemgetter
+
+try:
+    with open ("tasks.json", "r") as my_file:
+        my_list = json.load(my_file)
+except json.JSONDecodeError:
+    my_list = []
+
+
+
+def adding_task()->None:
+    my_task = input("What task would you like to add? ")
+    task_category = input("What is the category of this task? (Work/Personal) ")
+    due_date = input("What is a due date for this task? (e.g 05.04.24) ")
+    priority = int(input("How would you like to prioritize this task? (1 = High, 2 = Medium, 3= Low) "))
+    my_list.append({"Task":my_task.lower(), "Category":task_category, "Due date":due_date,"Priority":priority, "Done":False})
+    with open ("tasks.json", "w") as output:
+         json.dump(my_list, output, indent=4)
+
+def listing_tasks()->None:
+     with open("tasks.json", "r") as file:
+        my_list = json.load(file)
+        for i in my_list:
+            if i["Done"] == False:
+                print(i["Task"],"[]")
+            elif i["Done"] ==True:
+                print(i["Task"],"[✔]")
+
+
+def removing_tasks(task_index:int)->None:
+    my_list.pop(task_index-1)
+    with open ("tasks.json", "w") as my_file:
+        json.dump(my_list, my_file)
+
+
+def completing_task(task_index:int)->None:
+    my_list[task_index-1]["Done"] =True
+    with open ("tasks.json", "w") as my_file:
+        json.dump(my_list, my_file)
+
+
+
+def sorting_by_rating()->None:
+    sorted_tasks = sorted(my_list, key=itemgetter("Priority"))
+    print(sorted_tasks)
+
+def finding_tasks()->None:
+    desired_task = input("What task you are interested in? ").lower
+    try:
+        for task in my_list:
+            if task["Task"]==desired_task:
+                print(task)
+    except Exception as e:
+        print(e)
+
+
---
+++ PTUA17/atsiskaitymas/main.py
@@ -0,0 +1,42 @@
+from funcs import *
+
+try:
+    with open ("tasks.json", "r") as my_file:
+        my_list = json.load(my_file)
+except json.JSONDecodeError:
+    my_list = []
+
+
+def list_manager():
+    while True:
+        choice = int(input(("===== To-Do List Manager ===== \n 1.Add task \n 2.View tasks \n 3.Mark task as completed \n 4.Remove task \n 5.Sort tasks by their rating \n 6.Find a task using its name \n 7.Exit \n Enter your choice: ")))
+        try:
+
+         if choice == 1:
+            try:
+                adding_task()
+            except ValueError:
+                print("Failed to add, make sure that input an integer for Priority(1 = High, 2 = Medium, 3 = Low)")
+         elif choice ==2:
+                listing_tasks()
+         elif choice ==3:
+                task_index = int(input(f"Which task would you like to mark as completed? Enter index of the task (starting from 1) {my_list} "))
+                completing_task(task_index)
+                print(f"Task nr. {task_index} was sucesffully completed")
+         elif choice ==4:
+                task_index = int(input("Which task would you like to remove? "))
+                removing_tasks(task_index)
+         elif choice == 5:
+            sorting_by_rating()
+         elif choice ==6:
+            finding_tasks()
+         elif choice ==7:
+            break
+         else:
+             print("Number must be between 1 and 7")
+        except Exception as e:
+           print(f"Operation can not be completed because of {e}")
+
+
+
+list_manager()
