eb38a90d538e40b89bd908a18864bb7b2a1963b0
Arnas <arnas.zmejauskas1@gmail.com>
2025-03-18 17:58:48 +0200

    nc

--- PTUA17/objektinis/paveldejimai.py
+++ PTUA17/objektinis/paveldejimai.py
@@ -8,26 +8,36 @@ class Gun:
         self.__where_to_buy = "Go to afghanistan"
         self.is_cheap = is_cheap
 
-    def guns_data(self):
+    def main_data(self):
         print(f"Name: {self.name}, Country of origin: {self.country}, Reliable: {self.is_reliable}, Cheap: {self.is_cheap}")
         
 
 class Auto(Gun):
-    def __init__(self):
-        super().__init__("AK", "russia", True, False)
+    def __init__(self, name, country, is_reliable, is_cheap, is_old):
+        self.is_old = is_old
+        super().__init__(name, country, is_reliable, is_cheap)
+
+    def sub_data(self):
+        print(f"Name: {self.name}, Country of origin: {self.country}, Reliable: {self.is_reliable}, Cheap: {self.is_cheap}, Old: {self.is_old}")
 
 class Semi(Gun):
-    def __init__(self):
-        super().__init__("Glock", "Belgium", True, True)
+    def __init__(self, name, country, is_reliable, is_cheap, mag_size):
+        self.mag_size = mag_size
+        super().__init__(name, country, is_reliable, is_cheap)
+    def sub_data(self):
+        print(f"Name: {self.name}, Country of origin: {self.country}, Reliable: {self.is_reliable}, Cheap: {self.is_cheap}, Magazine capacity: {self.mag_size}")
 
-        pass
 
 
 
+auto_gun = Auto("AK", "russia", True, False, True)
+auto_gun.sub_data()
+auto_gun.main_data()
 
-my_guns = Auto()
-my_guns.guns_data()
+semi_gun = Semi("Glock", "Belgium", True, True, 15)
+semi_gun.sub_data()
+semi_gun.main_data()
 
-glock = Semi()
-glock.guns_data()
+# glock = Semi()
+# glock.guns_data()
 
