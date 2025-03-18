9e84ccf2ee6d9e8cfd4feb3eeb151c9523ead3a8
Arnas <arnas.zmejauskas1@gmail.com>
2025-03-18 17:41:57 +0200

    f

--- PTUA17/objektinis/paveldejimai.py
+++ PTUA17/objektinis/paveldejimai.py
@@ -1,22 +1,33 @@
 
 
 class Gun:
-    def __init__(self, name:str, country:str, is_realiable:bool):
+    def __init__(self, name:str, country:str, is_realiable:bool, is_cheap:bool):
         self.name = name
         self.country = country
         self.is_reliable = is_realiable
-
-class Auto(Gun):
-    def __init__(self, is_cheap):
+        self.__where_to_buy = "Go to afghanistan"
         self.is_cheap = is_cheap
 
     def guns_data(self):
-        super().__init__("ak", "russia", True)
         print(f"Name: {self.name}, Country of origin: {self.country}, Reliable: {self.is_reliable}, Cheap: {self.is_cheap}")
+        
+
+class Auto(Gun):
+    def __init__(self):
+        super().__init__("AK", "russia", True, False)
+
+class Semi(Gun):
+    def __init__(self):
+        super().__init__("Glock", "Belgium", True, True)
+
+        pass
 
 
 
 
-my_guns = Auto(False)
+my_guns = Auto()
 my_guns.guns_data()
+
+glock = Semi()
+glock.guns_data()
 
