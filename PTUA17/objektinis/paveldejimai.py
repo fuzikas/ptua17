2608ebda90eff677ebc1c199dcf061ffa22e2f83
Arnas <arnas.zmejauskas1@gmail.com>
2025-03-18 18:33:47 +0200

    votak

--- PTUA17/objektinis/paveldejimai.py
+++ PTUA17/objektinis/paveldejimai.py
@@ -5,8 +5,8 @@ class Gun:
         self.name = name
         self.country = country
         self.is_reliable = is_realiable
-        self.__where_to_buy = "Go to afghanistan"
         self.is_cheap = is_cheap
+        self.__where_to_buy = "Go to afghanistan"
 
     def main_data(self):
         print(f"Name: {self.name}, Country of origin: {self.country}, Reliable: {self.is_reliable}, Cheap: {self.is_cheap}")
@@ -26,6 +26,12 @@ class Semi(Gun):
         super().__init__(name, country, is_reliable, is_cheap)
     def sub_data(self):
         print(f"Name: {self.name}, Country of origin: {self.country}, Reliable: {self.is_reliable}, Cheap: {self.is_cheap}, Magazine capacity: {self.mag_size}")
+    
+    def buy(self):
+        try:
+            print(f"In order to buy any of these guns without a permit you should go to {self.__where_to_buy}")
+        except AttributeError:
+            print("You are not allowed to know this")
 
 
 
@@ -36,8 +42,25 @@ auto_gun.main_data()
 
 semi_gun = Semi("Glock", "Belgium", True, True, 15)
 semi_gun.sub_data()
-semi_gun.main_data()
+semi_gun.buy()
 
-# glock = Semi()
-# glock.guns_data()
+
+
+class Food:
+    def __init__(self, name:str, is_healthy:bool, is_exoctic:bool)->None:
+        self.name = name
+        self.is_healthy = is_healthy
+        self.is_exotic = is_exoctic
+
+class Carbs(Food):
+    def __init__(self, name:str, is_healthy:bool, is_exotic:bool, rich_in_iron:bool, rich_in_fiber:bool)->None:
+        self.rich_in_iron = rich_in_iron
+        self.rich_in_fiber = rich_in_fiber
+        super().__init__(name, is_healthy, is_exotic)
+    def show_data(self):
+        print(f"Name: {self.name}, Healthy: {self.is_healthy}, Exotic: {self.is_exotic}, Is rich in iron {self.rich_in_iron}, Is rich in fiber: {self.rich_in_fiber} ")
+
+
+my_food =Carbs("banana", True, True, True, True)
+my_food.show_data()
 
